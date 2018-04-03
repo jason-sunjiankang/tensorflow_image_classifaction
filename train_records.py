3bc3c6ca79f0c4e5504a65116631b94667f855f1
jason-sunjiankang <ytusunjiankang@163.com>
2018-04-03 14:22:22 +0800

    -madd comments

--- train_records.py
+++ train_records.py
@@ -6,7 +6,7 @@ import os
 import numpy as np
 import tensorflow as tf
 import model
-import creat_records as cr
+import creat_tfrecords as cr
 
 N_CLASSES = 2
 IMG_W = 64 #resize the image, if the input image is too large, training will be very slow.  
@@ -16,12 +16,9 @@ CAPACITY = 2000
 MAX_STEP = 500 #with current parameters, it is suggested to use MAX_STEP>10k
 learning_rate = 0.0001 #with current parameters, it is suggested to use learning rate<0.0001
 
-
-def run_training1():
-
-    logs_train_dir = "D:/sunjiankang/tensorflow_model/" #*********************************need to modify**************************************
-    
-    tfrecords_file = "D:/sunjiankang/tensorflow_model/dogCat_model/a.tfrecords" #*********************************need to modify**************************************
+#logs_train_dir: log save path
+#tfrecords_file: the path tfrecords save
+def run_training(logs_train_dir, tfrecords_file):
     train_batch, train_label_batch = cr.read_and_decode(tfrecords_file, batch_size=BATCH_SIZE)
     train_batch = tf.cast(train_batch, dtype=tf.float32) #数据格式转换
     train_label_batch = tf.cast(train_label_batch, dtype=tf.int64)
@@ -32,7 +29,7 @@ def run_training1():
     train__acc = model.evaluation(train_logits, train_label_batch)
 
     
-    summary_op = tf.summary.merge_all() #获取所有监测的操作
+    summary_op = tf.summary.merge_all() #get all monitored operations
     sess = tf.Session()
     train_writer = tf.summary.FileWriter(logs_train_dir, sess.graph)#生成一个写日志的writer，并将当前的计算图写入日志
     saver = tf.train.Saver()
@@ -49,8 +46,8 @@ def run_training1():
             if step % 10 == 0:
                 print("**********************")
                 print("Step %d, train loss = %.5f, train accuracy = %.2f%%" %(step, tra_loss, tra_acc*100.0))
-                summary_str = sess.run(summary_op) #获取监测结果
-                train_writer.add_summary(summary_str, step) #写入文件
+                summary_str = sess.run(summary_op) #get monitoring results
+                train_writer.add_summary(summary_str, step) #write file
                 
 
             if step % 2000 == 0 or (step +1) == MAX_STEP:
@@ -66,8 +63,9 @@ def run_training1():
     coord.join(threads)
     sess.close()
 
-
-
-run_training1()
+#*******************************training**********************************
+logs_train_dir = "D:/sunjiankang/tensorflow_model/" #*********************************need to modify************************************** 
+tfrecords_file = "D:/sunjiankang/tensorflow_model/dogCat_model/a.tfrecords" #*********************************need to modify**************************************
+run_training(logs_train_dir, tfrecords_file)
 
 
